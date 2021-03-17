650141ffa9be03a65260cf463d578f4085137c6a
threader <40972803+localthreader@users.noreply.github.com>
2021-03-17 11:47:58 -0300

    Adicionando Mixin que não permite jogadores atirarem com skins que não possuem.

---
+++ src/main/java/org/armacraft/mod/mixin/GunImplMixin.java
@@ -0,0 +1,46 @@
+package org.armacraft.mod.mixin;
+
+import com.craftingdead.core.capability.ModCapabilities;
+import com.craftingdead.core.capability.gun.GunImpl;
+import com.craftingdead.core.capability.living.ILiving;
+import com.craftingdead.core.item.PaintItem;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Util;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import org.armacraft.mod.ArmaCraft;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GunImpl.class)
+public class GunImplMixin {
+
+    @Shadow @Final protected ItemStack gunStack;
+
+    @Inject(method = "processShot", remap=false, at = @At("TAIL"))
+    private void processShot(ILiving<?, ?> living, CallbackInfo ci) {
+        if(living.getEntity() instanceof PlayerEntity) {
+            PlayerEntity playerEntity = (PlayerEntity) living.getEntity();
+            gunStack.getCapability(ModCapabilities.GUN).ifPresent(gunController -> {
+                if(gunController.getPaint().isPresent()) {
+                    PaintItem paint = (PaintItem) gunController.getPaintStack().getItem();
+                    String permissionNode = "armacraft.skins."
+                            + gunStack.getItem().getRegistryName().toString().replaceAll("^craftingdead:", "")
+                            + "." + paint.getRegistryName().toString().replaceAll("^craftingdead:", "");
+                    if (!ArmaCraft.PERMISSION_CHECKER.checkPermission(playerEntity.getUniqueID(), permissionNode)) {
+                        playerEntity.sendMessage(new TranslationTextComponent("message.no_skin_permission")
+                                .setStyle(Style.EMPTY.applyFormatting(TextFormatting.RED).setBold(true)), Util.DUMMY_UUID);
+                        gunStack.getCapability(ModCapabilities.GUN).ifPresent(x -> x.setPaintStack(ItemStack.EMPTY));
+                    }
+                }
+            });
+        }
+    }
+
+}
