68e5feb3ef79f533f07edb4a11d20f8c94ffa6d3
André Sales <andre.sales.est@gmail.com>
2021-04-11 04:05:48 -0300

    Resolve warning ao abrir em server

--- src/main/java/org/armacraft/mod/mixin/MinecraftMixin.java
+++ src/main/java/org/armacraft/mod/mixin/MinecraftMixin.java
@@ -6,8 +6,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
@@ -16,7 +14,6 @@ public class MinecraftMixin {
 	 * Seta o titulo do jogo pro nosso proprio
 	 */
 	@Inject(method = "createTitle", at = @At("RETURN"), cancellable = true)
-	@OnlyIn(Dist.CLIENT)
 	public void createTitle(CallbackInfoReturnable<String> a) {
 		a.setReturnValue("ARMACRAFT - "+a.getReturnValue());
 	}
