a19e3d9695f9cc8d69fe254ce25a3b6b1f82d534
André Sales <andre.sales.est@gmail.com>
2021-04-11 04:07:35 -0300

    Reforça logica do anticheat

--- src/main/java/org/armacraft/mod/network/ResponseModsPacket.java
+++ src/main/java/org/armacraft/mod/network/ResponseModsPacket.java
@@ -1,6 +1,8 @@
 package org.armacraft.mod.network;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Supplier;
@@ -9,18 +11,18 @@ import org.armacraft.mod.ArmaCraft;
 import org.armacraft.mod.util.MiscUtil;
 
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.server.dedicated.DedicatedServer;
 import net.minecraftforge.fml.network.NetworkEvent;
-import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 public class ResponseModsPacket {
 
 	private Map<String, String> hashes;
-	
-	private ResponseModsPacket(Map<String, String> hashes) {
+	private List<String> transformationServices;
+
+	private ResponseModsPacket(Map<String, String> hashes, List<String> transformationServices) {
 		this.hashes = hashes;
+		this.transformationServices = transformationServices;
 	}
-	
+
 	public static void encode(ResponseModsPacket msg, PacketBuffer out) {
 		out.writeByte(msg.hashes.size());
 
@@ -28,22 +30,36 @@ public class ResponseModsPacket {
 			out.writeUtf(entry.getKey());
 			out.writeUtf(entry.getValue());
 		});
+
+		out.writeByte(msg.transformationServices.size());
+
+		msg.transformationServices.forEach(out::writeUtf);
 	}
 
 	public static ResponseModsPacket decode(PacketBuffer in) {
 		ResponseModsPacket modsPacket = ResponseModsPacket.empty();
 
-		byte amount = in.readByte();
+		byte modsAmount = in.readByte();
 
-		if (amount > 50) {
-			throw new RuntimeException("Too many mods: "+amount);
+		if (modsAmount > 20) {
+			throw new RuntimeException("Too many mods: " + modsAmount);
 		}
 
-		for (int i = 0; i < amount; i++) {
+		for (int i = 0; i < modsAmount; i++) {
 			String modId = in.readUtf(100);
 			String hash = in.readUtf(32);
 
 			modsPacket.hashes.put(modId, hash);
+		}
+
+		byte transformationServices = in.readByte();
+
+		if (transformationServices > 10) {
+			throw new RuntimeException("Too many transformation services: " + transformationServices);
+		}
+
+		for (int i = 0; i < modsAmount; i++) {
+			modsPacket.transformationServices.add(in.readUtf(100));
 		}
 
 		return modsPacket;
@@ -52,38 +68,46 @@ public class ResponseModsPacket {
 	public static boolean handle(ResponseModsPacket msg, Supplier<NetworkEvent.Context> ctx) {
 		if (ctx.get().getDirection().getReceptionSide().isServer()) {
 			ctx.get().enqueueWork(() -> {
-				
+
 				final String playerName = ctx.get().getSender().getGameProfile().getName();
-				
-				DedicatedServer server = (DedicatedServer) ServerLifecycleHooks.getCurrentServer();
-				
+
 				Map<String, String> serverHashes = ArmaCraft.getInstance().getServerDist().getHashes();
+				
+				// Valida hashes
 				for (Entry<String, String> entry : msg.hashes.entrySet()) {
-					String clientModId = entry.getKey();
+					String clientFileName = entry.getKey();
 					String clientModHash = entry.getValue();
 
-					String theModHashInServer = serverHashes.get(clientModId);
+					String theModHashInServer = serverHashes.get(clientFileName);
 
 					// Mod existe no server
 					if (theModHashInServer != null) {
 						// Hash n�o bate
 						if (!clientModHash.equals(theModHashInServer)) {
-							server.runCommand("delegatemodmismatch "+playerName+" "+clientModId+" "+clientModHash);
+							MiscUtil.runConsoleCommand(
+									"delegatemodinfo hashmismatch " + playerName + " " + clientFileName + " " + clientModHash);
 						}
 					} else {
-						server.runCommand("delegateunknownmod "+playerName+" "+clientModId+" "+clientModHash);
+						MiscUtil.runConsoleCommand(
+								"delegatemodinfo unknownfile " + playerName + " " + clientFileName + " " + clientModHash);
 					}
+				}
+				
+				// Recebe os transformation services que nao sao exatamente mods (Optifine, etc...)
+				for (String transformationService : msg.transformationServices) {
+					MiscUtil.runConsoleCommand(
+							"delegatemodinfo tfservice " + playerName + " " + transformationService);
 				}
 			});
 		}
 		return true;
 	}
-	
+
 	public static ResponseModsPacket empty() {
-		return new ResponseModsPacket(new HashMap<>());
+		return new ResponseModsPacket(new HashMap<>(), new ArrayList<>());
 	}
-	
+
 	public static ResponseModsPacket withMyMods() {
-		return new ResponseModsPacket(MiscUtil.calculateMyHashes());
+		return new ResponseModsPacket(MiscUtil.calculateMyHashes(), MiscUtil.getTransformationServices());
 	}
 }
--- src/main/java/org/armacraft/mod/util/MiscUtil.java
+++ src/main/java/org/armacraft/mod/util/MiscUtil.java
@@ -5,33 +5,54 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.google.common.hash.Hashing;
 import com.google.common.io.Files;
 
+import cpw.mods.modlauncher.Launcher;
+import cpw.mods.modlauncher.api.IEnvironment;
 import net.minecraft.entity.Entity;
+import net.minecraft.server.dedicated.DedicatedServer;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 public class MiscUtil {
-	
+
+	/**
+	 * Optifine eh um exemplo de tranformation service. Ele nao aparece na lista de
+	 * mods do Forge.
+	 */
+	public static List<String> getTransformationServices() {
+		final List<Map<String, String>> modList = Launcher.INSTANCE.environment()
+				.getProperty(IEnvironment.Keys.MODLIST.get()).orElseThrow(() -> new RuntimeException("Not set"));
+
+		return modList.stream().filter(map -> {
+			if (!"TRANSFORMATIONSERVICE".equals(map.get("type"))) {
+				return false;
+			}
+
+			return true;
+		}).map(map -> map.get("name")).collect(Collectors.toList());
+	}
+
 	public static Map<String, String> calculateMyHashes() {
 		Map<String, String> hashes = new HashMap<>();
 
 		List<ModInfo> infos = FMLLoader.getLoadingModList().getMods();
 
-		infos.stream().forEach(modFileInfo -> {
-			File file = modFileInfo.getOwningFile().getFile().getFilePath().toFile();
-			
+		infos.stream().forEach(modInfo -> {
+			File file = modInfo.getOwningFile().getFile().getFilePath().toFile();
+
 			// Not a folder (for example, when running in the dev workspace)
 			if (file.isFile()) {
 				try {
-					String modId = modFileInfo.getModId();
 					String hash = Files.hash(file, Hashing.md5()).toString();
-					
-					hashes.put(modId, hash);
+
+					hashes.put(file.getName(), hash);
 				} catch (IOException e) {
 					throw new RuntimeException(e);
 				}
@@ -40,7 +61,13 @@ public class MiscUtil {
 
 		return hashes;
 	}
-	
+
+	public static void runConsoleCommand(String command) {
+		DedicatedServer server = (DedicatedServer) ServerLifecycleHooks.getCurrentServer();
+
+		server.getCommands().performCommand(server.createCommandSourceStack(), command);
+	}
+
 	public static void playSoundAtEntity(Entity entity, SoundEvent sound, float volume, float pitch) {
 		entity.level.playSound(null, entity.blockPosition(), sound, SoundCategory.HOSTILE, volume, pitch);
 	}
