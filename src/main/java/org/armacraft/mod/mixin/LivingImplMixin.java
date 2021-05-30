2ddc05d0a22d73b3205dc7016e2a10a06cecce39
threader <40972803+localthreader@users.noreply.github.com>
2021-05-30 15:55:12 -0300

    Corrigindo bug do colete sumindo em mundos com keep inventory

--- src/main/java/org/armacraft/mod/mixin/LivingImplMixin.java
+++ src/main/java/org/armacraft/mod/mixin/LivingImplMixin.java
@@ -1,20 +1,43 @@
 package org.armacraft.mod.mixin;
 
+import com.craftingdead.core.living.ILiving;
+import com.craftingdead.core.living.ILivingExtension;
 import com.craftingdead.core.living.LivingImpl;
 import com.craftingdead.core.util.ModSoundEvents;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.world.GameRules;
+import net.minecraftforge.items.ItemStackHandler;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+import java.util.Collection;
+
 @Mixin(LivingImpl.class)
-public abstract class LivingImplMixin<T extends LivingEntity> {
+public abstract class LivingImplMixin<L extends LivingEntity, E extends ILivingExtension>
+        implements ILiving<L, E> {
 
     /*
      * Desativa deitar
+     */
+    @Shadow @Final protected Object2ObjectOpenHashMap<ResourceLocation, E> extensions;
+
+    @Shadow public abstract L getEntity();
+
+    @Shadow @Final private ItemStackHandler itemHandler;
+
+    /**
+     * @author
      */
     @Overwrite(remap = false)
     public void setCrouching(boolean crouching, boolean sendUpdate) {
--- src/main/java/org/armacraft/mod/mixin/PlayerImplMixin.java
+++ src/main/java/org/armacraft/mod/mixin/PlayerImplMixin.java
@@ -1,13 +1,19 @@
 package org.armacraft.mod.mixin;
 
+import com.craftingdead.core.living.IPlayer;
+import com.craftingdead.core.living.IPlayerExtension;
+import com.craftingdead.core.living.LivingImpl;
 import com.craftingdead.core.living.PlayerImpl;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerImpl.class)
-public class PlayerImplMixin {
+public abstract class PlayerImplMixin<L extends PlayerEntity> extends LivingImpl<L, IPlayerExtension>
+		implements IPlayer<L> {
 
 	/**
 	 * Impede que a perna seja quebrada
@@ -15,5 +21,21 @@ public class PlayerImplMixin {
 	@Inject(method = "updateBrokenLeg", remap = false, at = @At("HEAD"), cancellable = true)
 	public void updateBrokenLeg(CallbackInfo ci) {
 		ci.cancel();
+	}
+
+	/**
+	 * @author
+	 */
+	@Overwrite(remap = false)
+	public void copyFrom(IPlayer<?> that, boolean wasDeath) {
+		// Copies the inventory. Doesn't actually matter if it was death or not.
+		// Death drops from 'that' should be cleared on death drops to prevent item duplication.
+		for (int i = 0; i < 5; i++) {
+			this.getItemHandler().setStackInSlot(i, that.getItemHandler().getStackInSlot(i));
+		}
+
+		for (IPlayerExtension extension : this.extensions.values()) {
+			extension.copyFrom(that, wasDeath);
+		}
 	}
 }
